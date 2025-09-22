2ff86ba96ecfdf6624b3160c47d4e87baae5fef4
Emad Gerami <emad.grnd@gmail.com>
2025-09-23 01:06:36 +0330

    Add first impression of Entities

---
+++ Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        prop
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
